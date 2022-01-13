a6d04d579b5cb5046e84a678083b2817abe57344
Mark Lawson <mark@walkonmars.co.uk>
2022-01-13 10:27:35 +0000

    changed variable names in js files

---
+++ index.js
@@ -0,0 +1,6 @@
+// import alphabet and morse objects from alphabet.js
+import {englishToMorse, morseToEnglish} from "./alphabetMorse.js";
+
+// check with console logs
+console.log(englishToMorse);
+console.log(morseToEnglish);
